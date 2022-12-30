01beb840255ff1ae3be5adf5547e364fb5f4614f
weiwuer xyz <weiwuer00@gmail.com>
2022-12-30 13:06:43 -0400

    weiwuer_dotfiles_backup.sh: init.

---
+++ weiwuer_dotfiles_backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+dotfiles_dir="$HOME/dotfiles"
+
+files=(
+".config/i3/config"
+".bashrc"
+".bash_aliases"
+".bash_profile"
+)
+
+[[ ! -d "$dotfiles_dir" ]] \
+	&& mkdir -p "$dotfiles_dir"\
+	&& printf "mkdir $dotfiles_dir: $?\n"
+
+for file in ${files[@]}; do
+	cp -r $HOME/$file $dotfiles_dir/$file 2>/dev/null
+	notify-send "$?: $dotfiles_dir/$file"
+done
